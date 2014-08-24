119328a0077cd032253e2b7410c84a9d5a0569c6
AnushaNilapu <anilapu@navaratan.com>
2014-08-24 12:23:46 +0530

    removed un necessary file and using score button to see score

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 /* defining the app */
 var app = angular
-	.module("quizApp", ['quizzes','ngResource','timer'])
+	.module("quizApp", ['quizzes','ngResource'])
 	.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
